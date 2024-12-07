510d9a645b895d56d749d3e032d91b03069e05f4
irfanfaiz25 <ir.faiz2000@gmail.com>
2024-12-07 11:07:20 +0700

    feat: create skills json data

---
+++ src/assets/data/skills.js
@@ -0,0 +1,65 @@
+import LaravelLogo from "../svg/laravel.svg";
+import PHPLogo from "../svg/php-logo.svg";
+import JSLogo from "../svg/js-logo.svg";
+import TailwindLogo from "../svg/tailwind-logo.svg";
+import ReactLogo from "../svg/react-logo.svg";
+import GitLogo from "../svg/git-logo.svg";
+import PostmanLogo from "../svg/postman-logo.svg";
+import MysqlLogo from "../svg/mysql-logo.svg";
+import PostgresLogo from "../svg/postgres-logo.svg";
+
+export const SkillsData = {
+  "Backend Development": {
+    skills: [
+      {
+        name: "PHP",
+        logo: PHPLogo,
+      },
+      {
+        name: "Laravel",
+        logo: LaravelLogo,
+      },
+    ],
+  },
+  "Database Management": {
+    skills: [
+      {
+        name: "MySQL",
+        logo: MysqlLogo,
+      },
+      {
+        name: "Postgresql",
+        logo: PostgresLogo,
+      },
+    ],
+  },
+  "Frontend Development": {
+    skills: [
+      {
+        name: "Javascript",
+        logo: JSLogo,
+      },
+      {
+        name: "ReactJS",
+        logo: ReactLogo,
+        borderColor: "border-t-tertiary-border",
+      },
+      {
+        name: "Tailwind",
+        logo: TailwindLogo,
+      },
+    ],
+  },
+  "Tools and Technologies": {
+    skills: [
+      {
+        name: "Git",
+        logo: GitLogo,
+      },
+      {
+        name: "Postman",
+        logo: PostmanLogo,
+      },
+    ],
+  },
+};
